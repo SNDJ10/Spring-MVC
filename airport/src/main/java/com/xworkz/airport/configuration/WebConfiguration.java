6f28f09c507fed4f807f98bfcdd84043148d8c36
Sanjay D <sanjayd143.xworkz@gmail.com>
2026-01-07 22:35:46 +0530

    maven tools

---
+++ airport/src/main/java/com/xworkz/airport/configuration/WebConfiguration.java
@@ -0,0 +1,7 @@
+package com.xworkz.airport.configuration;
+
+public class WebConfiguration{
+    public WebConfiguration(){
+        System.out.println("this is web configuration");
+    }
+}
---
+++ fooddetails/src/main/java/com/xworkz/fooddetails/configuration/WebConfiguration.java
@@ -0,0 +1,7 @@
+package com.xworkz.fooddetails.configuration;
+
+public class WebConfiguration {
+    public WebConfiguration(){
+        System.out.println("this is web configuration");
+    }
+}
